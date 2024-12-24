d48b00537a8d845ab18ceb2872e40d6506ae99bb
[200~2110107@newuu.uz <2110107@newuu.uz>
2024-12-24 11:20:34 +0500

    chapter3 in progress

--- src/main/java/Chapter3/sample/Sample.java
+++ src/main/java/Chapter3/sample/Sample.java
@@ -1,29 +1,14 @@
 package Chapter3.sample;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Sample {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        System.out.println(list.add("Rose"));
-        System.out.println(list.add("Rose"));
-
-        Set<String> stringSet = new HashSet<>();
-        System.out.println(stringSet.add("Cool"));
-        System.out.println(stringSet.add("Cool"));
-
-        List<String> birds = new ArrayList<>();
-        birds.add("hawk1");
-        birds.add("hawk");
-        System.out.println(birds.remove(0));
-        System.out.println(birds);
-    }
-
-    public static <T> Crate<T> ship(T t) {
-        System.out.println("Preparing " + t);
-        return new Crate<T>();
+        NavigableSet<Integer> set = new TreeSet<>();
+        for (int i = 1; i <= 20; i++) {
+            set.add(i);
+        }
+        System.out.println(set.lower(10));
+        
     }
 }
